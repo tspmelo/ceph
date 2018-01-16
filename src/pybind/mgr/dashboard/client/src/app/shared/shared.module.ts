3078b75e1b8c9e9acf5d591fd6f8f568b3e2dcd1
Tiago Melo <tmelo@suse.com>
2018-01-17 00:10:55 +0100

    Revert "add ngx-datatable"
    This reverts commit 9f0b489cd596bf9cfcb3e6ce6cef372fd7224758.

--- src/pybind/mgr/dashboard/client/src/app/shared/shared.module.ts
+++ src/pybind/mgr/dashboard/client/src/app/shared/shared.module.ts
@@ -2,19 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PipesModule } from './pipes/pipes.module';
 import { ServicesModule } from './services/services.module';
-import { ComponentsModule } from './components/components.module';
 
 @NgModule({
   imports: [
     CommonModule,
     PipesModule,
-    ServicesModule,
-    ComponentsModule
+    ServicesModule
   ],
   exports: [
     PipesModule,
-    ServicesModule,
-    ComponentsModule
+    ServicesModule
   ],
   declarations: []
 })
