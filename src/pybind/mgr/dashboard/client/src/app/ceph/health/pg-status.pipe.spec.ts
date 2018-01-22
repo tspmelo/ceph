554bb694b4cb5ed2dcb558ee999709395c0b5d23
Stephan Müller <smueller@suse.com>
2018-01-22 16:16:58 +0100

    Enable the unit tests to be run
    Two unit test had to be commented out because the automatically created
    tests weren't complete and crashed any attempt to run the unit tests. I
    added two unit tests to confirm the operator change in the pipe. I found
    out that all component tests will fail because the imports hadn't been
    add automatically to the test suite on creation.
    
    Signed-off-by: Stephan Müller <smueller@suse.com>

--- src/pybind/mgr/dashboard/client/src/app/ceph/health/pg-status.pipe.spec.ts
+++ src/pybind/mgr/dashboard/client/src/app/ceph/health/pg-status.pipe.spec.ts
@@ -1,8 +1,21 @@
 import { PgStatusPipe } from './pg-status.pipe';
 
 describe('PgStatusPipe', () => {
+  let pipe: PgStatusPipe;
+
+  beforeEach(() => {
+    pipe = new PgStatusPipe();
+  });
+
   it('create an instance', () => {
-    const pipe = new PgStatusPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it ('should not fail with not input', () => {
+    expect(pipe.transform('')).toBe('');
+  });
+
+  it ('should show count and status in the right order', () => {
+    expect(pipe.transform({someStatus: 42})).toBe('42 someStatus');
   });
 });
--- src/pybind/mgr/dashboard/client/src/app/shared/pipes/dimless.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DimlessPipe } from './dimless.pipe';
-
-describe('DimlessPipe', () => {
-  it('create an instance', () => {
-    const pipe = new DimlessPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
