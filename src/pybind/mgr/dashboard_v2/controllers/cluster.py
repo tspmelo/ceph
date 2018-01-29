4af32135c234e500deb46426ca9412a5d7037b36
Sebastian Wagner <sebastian.wagner@suse.com>
2018-01-29 12:22:49 +0100

    Added very basic and not yet usable REST controller for CephCluster
    Signed-off-by: Sebastian Wagner <sebastian.wagner@suse.com>

---
+++ src/pybind/mgr/dashboard_v2/controllers/cluster.py
@@ -0,0 +1,29 @@
+from functools import partial
+
+from ..models.nodb import nodb_context
+from ..models.cluster import CephCluster
+from ..tools import ApiController, RESTController
+
+
+def nodb_serializer(model, obj):
+    # pylint: disable=W0212
+    return {field.attname: getattr(obj, field.attname) for field in model._meta.fields}
+
+
+cluster_serializer = partial(nodb_serializer, CephCluster)
+
+
+@ApiController('cluster')
+class Cluster(RESTController):
+
+    def __init__(self):
+        self._mod = Cluster._mgr_module_
+        self._log = self._mod.log
+
+    def list(self):
+        with nodb_context(self):
+            map(cluster_serializer, CephCluster.objects.all())
+
+    def get(self, _):
+        with nodb_context(self):
+            return cluster_serializer(CephCluster.objects.get())
