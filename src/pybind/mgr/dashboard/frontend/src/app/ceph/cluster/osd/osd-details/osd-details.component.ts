e7c4a235a38e4effc552e7ca857b244c1c222360
Tiago Melo <tmelo@suse.com>
2018-05-17 10:43:55 +0100

    mgr/dashboard: Improve loading speed of details tabs
    Signed-off-by: Tiago Melo <tmelo@suse.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/cluster/osd/osd-details/osd-details.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/cluster/osd/osd-details/osd-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy } from '@angular/core';
 
 import * as _ from 'lodash';
 
@@ -10,10 +10,12 @@ import { CdTableSelection } from '../../../../shared/models/cd-table-selection';
   templateUrl: './osd-details.component.html',
   styleUrls: ['./osd-details.component.scss']
 })
-export class OsdDetailsComponent implements OnChanges {
+export class OsdDetailsComponent implements OnChanges, OnDestroy {
   @Input() selection: CdTableSelection;
 
   osd: any;
+  tab = 'Attributes';
+  interval: any;
 
   constructor(private osdService: OsdService) {}
 
@@ -23,22 +25,26 @@ export class OsdDetailsComponent implements OnChanges {
     };
     if (this.selection.hasSelection) {
       this.osd = this.selection.first();
-      this.osd.autoRefresh = () => {
-        this.refresh();
-      };
       this.refresh();
+      clearInterval(this.interval);
+      this.interval = setInterval(() => {
+        this.refresh();
+      }, 5000);
     }
   }
 
+  ngOnDestroy() {
+    clearInterval(this.interval);
+  }
+
   refresh() {
-    this.osdService.getDetails(this.osd.tree.id)
-      .subscribe((data: any) => {
-        this.osd.details = data;
-        if (!_.isObject(data.histogram)) {
-          this.osd.histogram_failed = data.histogram;
-          this.osd.details.histogram = undefined;
-        }
-        this.osd.loaded = true;
-      });
+    this.osdService.getDetails(this.osd.tree.id).subscribe((data: any) => {
+      this.osd.details = data;
+      if (!_.isObject(data.histogram)) {
+        this.osd.histogram_failed = data.histogram;
+        this.osd.details.histogram = undefined;
+      }
+      this.osd.loaded = true;
+    });
   }
 }
