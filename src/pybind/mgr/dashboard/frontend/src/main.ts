6bfe7e0c11536e622a20203764b506abe8ff841e
Tiago Melo <tmelo@suse.com>
2018-11-28 12:27:31 +0000

    mgr/dashboard: Add HMR support
    Signed-off-by: Tiago Melo <tmelo@suse.com>

--- src/pybind/mgr/dashboard/frontend/src/main.ts
+++ src/pybind/mgr/dashboard/frontend/src/main.ts
@@ -4,13 +4,25 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { i18nProviders } from './app/locale.helper';
 import { environment } from './environments/environment';
+import { hmrBootstrap } from './hmr';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule, {
-    providers: i18nProviders
-  })
-  .catch((err) => console.log(err));
+const bootstrap = () =>
+  platformBrowserDynamic()
+    .bootstrapModule(AppModule, {
+      providers: i18nProviders
+    });
+
+if (environment.hmr) {
+  if (module['hot']) {
+    hmrBootstrap(module, bootstrap);
+  } else {
+    console.error('HMR is not enabled for webpack-dev-server!');
+    console.log('Are you using the --hmr flag for ng serve?');
+  }
+} else {
+  bootstrap().catch((err) => console.log(err));
+}
